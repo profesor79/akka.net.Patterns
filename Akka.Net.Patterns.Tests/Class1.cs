f13b0bedb0f542798d999b228fdaa94949339fc1
profesor79 <gbernas@API-LT-022>
2017-07-22 00:11:42 +0100

    initial commit for visual studio project

---
+++ Akka.Net.Patterns.Tests/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.Net.Patterns.Tests
+{
+    using Akka.Actor;
+    using Akka.TestKit;
+    using Akka.TestKit.Xunit2;
+
+    using NUnit.Framework;
+
+    public class Class1 : TestKit
+    {
+        private TestProbe _testProbe;
+
+        /// <summary>The no message sent.</summary>
+        /// <param name="i">The i.</param>
+        public void NoMessageSent(int i = 100)
+        {
+            ExpectNoMsg(TimeSpan.FromMilliseconds(i));
+            _testProbe.ExpectNoMsg(TimeSpan.FromMilliseconds(i));
+        }
+
+        /// <summary>The setup.</summary>
+        [SetUp]
+        public void Setup()
+        {
+            _testProbe = CreateTestProbe("testProbe");
+        }
+
+        /// <summary>The tear down.</summary>
+        [TearDown]
+        public void TearDown()
+        {
+            _testProbe.Tell(PoisonPill.Instance);
+
+        }
+    }
+}
---
+++ AkkaNet.Patterns/Program.cs
@@ -0,0 +1,24 @@
+﻿//  --------------------------------------------------------------------------------------------------------------------
+// <copyright company="profesor79.pl" file="Program.cs">
+// Copyright (c) 2017 All Right Reserved
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+// </copyright>
+// <summary>
+// Created: 2017-07-21, 11:50 PM
+// Last changed by: profesor79, 2017-07-22, 12:06 AM 
+// </summary>
+//   --------------------------------------------------------------------------------------------------------------------
+
+namespace AkkaNet.Patterns
+{
+    /// <summary>The program.</summary>
+    internal class Program
+    {
+        /// <summary>The main.</summary>
+        /// <param name="args">The args.</param>
+        private static void Main(string[] args) { }
+    }
+}
